b476f4df3d5ec7d4f2c449278299d209cb224828
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-09-30 10:45:34 +0900

    Update 15652.py

--- 15652.py
+++ 15652.py
@@ -1,32 +1,17 @@
-#오름차순일것
-#겹치는 부분이 엇ㅂ을것
-class Solution:
-    def permute(self, nums, count) :
-        results = [] #[[1,2][1,3]...]
-        
-        if len(nums) == 1 :
-            return [nums[:]]
-        
-        #execute for only two depth
-        for i in range(len(nums)) :
-            target = nums.pop(0)
-            
-            if count == 0:
-                return nums
-            else :
-                print("nums : ",nums)
-                perms = self.permute(nums,count-1)
-                print("perms : ",perms)
-                
-                perms.append(target)
-                
-                results.append(perms)
-            
-            nums.append(target)
-        
-        return results
-            
-            
+N, M = map(int, input().split(" "))
+
+result = []
+
+def sol(start) :
     
-answer = Solution().permute([1,2,3,4], 2)
-print(answer)
+    if len(result) == M :
+        print(*result)
+        return
+    
+    for i in range(start,N) :
+        result.append(i+1)
+        sol(i)
+        result.pop()
+    
+
+sol(0)
