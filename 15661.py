daa51bd14023fa8500b2091b3ea8eff5cc43a6d7
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-10-03 13:24:33 +0900

    Update 15661.py
    시간초과 코드

--- 15661.py
+++ 15661.py
@@ -1,28 +1,43 @@
-N = 4
-M = [
-        [0,1,2,3,4,5],
-        [1,0,2,3,4,5],
-        [1,2,0,3,4,5],
-        [1,2,3,0,4,5],
-        [1,2,3,4,0,5],
-        [1,2,3,4,5,0]
-    ]
+import math
+
+N = int(input())
+M = [ list(map(int,input().split(" "))) for _ in range(N) ]
 
 result = []
+min_ = float("inf")
 
+def cal(arr) :
+    sum_ = 0
+    
+    for i in range(len(arr)-1) :
+        for j in range(i+1,len(arr)) :
+            sum_ += M[arr[i]-1][arr[j]-1] + M[arr[j]-1][arr[i]-1]
+    return sum_
+
+            
+            
 def sol(idx, depth) :
+    global min_
  
     if depth == 0 :
-        print(*result)
+        opposite = []
+    
+        #opposite array 만들기
+        for i in range(N) :
+            if i+1 not in result :
+                opposite.append(i+1)
+                
+        min_ = min(min_,abs(cal(result)-cal(opposite)))
   
-    for i in range(idx+1,7) :
+    for i in range(idx+1,N+1) :
         result.append(i)
         sol(i, depth-1)
         result.pop()
-        
     
-   
     
-for i in range(2,5) :
-    print("comb ",i)
+for i in range(2,math.floor(N/2)+1) :
     sol(0,i)
+
+print(min_)
+
+    
