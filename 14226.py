4dea93a47d88faa6daa7d20a255d81d0e4677646
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-10-15 09:09:58 +0900

    Update 14226.py

--- 14226.py
+++ 14226.py
@@ -1,30 +1,45 @@
-S=6
-count = [0] * (S+1)
-visited = [0] * (S+1)
-clipboard = []
+#while True :
+S=int(input())
+MAX = 2001
+clipboard = [0] * MAX
+visited = [0] * MAX
+parent = [0] * MAX
 
 queue = []
 queue.append(1)
+parent[1] = 1
+visited[1] = 1
+clipboard[1] = 1
 
 while len(queue) > 0 :
     q = queue.pop(0)
+    visited[q] = 1
     if q == S :
         break
-        
-    clipboard.append(q)
-    count[q] += 1
     
-    for c in clipboard :
-        if q+c <= S and visited[q+c] == 0 :
+    #남의 클립보드를 함께 안써야 한다
+    for c in [clipboard[q], q] :
+        if  1<= q+c <= MAX and visited[q+c] == 0 :
             queue.append(q+c)
-            count[q+c] += 1
-            visited[q+c] = 1
+            if c == q :
+                visited[q+c] = 2
+            else :
+                visited[q+c] = 1
+            parent[q+c] = q
+            clipboard[q+c] = c
         
-    for k in queue :
-        if k - 1 > q and visited[k] == 0 :
-            queue.append(k-1)
-            count[k-1] += 1
+    if 1 <= q - 1 <= MAX and visited[q-1] == 0 :
+        queue.append(q-1)
+        visited[q-1] = 1
+        parent[q-1] = q
+        clipboard[q-1] = clipboard[q]      
     
-    print(q,"======>",queue, "\n")
+
+dest = S
+count = 0
+while dest > 1 :
+    #print(dest,":",visited[dest])
+    count += visited[dest]
+    dest = parent[dest]
 
 print(count)
