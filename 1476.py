8c5e3bc134d3621378efa868449793391bb817da
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-10-17 18:00:38 +0900

    Create 1476.py

---
+++ 1476.py
@@ -0,0 +1,23 @@
+n=0
+
+l,m,z = map(int,input().split(" "))
+
+earthHome = []
+sunHome = []
+moonHome = []
+
+found = False
+checked = []
+
+while found == False:
+    
+    earth = 15*n + l
+    sun = 28*n + m
+    moon = 19*n + z
+    
+    #sun 기준으로 계산
+            
+    n += 1
+    
+
+
