21a6e6587613de3ca8b7cf06509d68bb40f9a6db
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-12-05 08:21:24 +0900

    Update 6588.py
    -sys.stdin.readline
    - 에스트로네의 체인가 뭐시긴가
    - 소수검사할때 절반 + 1

--- 6588.py
+++ 6588.py
@@ -1,21 +1,23 @@
+import sys
 
-def isPrime(n) :
-    for i in range(2, int(n**0.5) +1 ) :
-        if n % i == 0 :
-            return False
-    return True
-    
+maxNum = 1000000
+
+primes = list(range(maxNum + 1))
+issqt = int(maxNum**0.5) + 1
+
+for i in range(2, issqt) :
+    if primes[i] :
+        primes[i+i::i] = [0] * len(primes[i+i::i])
+
 while True :
-    m0 = int(input())
+    m0 = int(sys.stdin.readline())
     
     if m0 == 0 :
         break
     
-    m1 = 2
-    while m1 > 1 :
-        if isPrime(m1) :
-            m2 = m0 - m1
-            if isPrime(m2):
-                print(f"{m0} = {m1} + {m2}")
-                break
-        m1 += 1
+    for m1 in range(3, int(m0 / 2) + 1, 2) :
+        if primes[m1] and primes[m0 - m1] :
+            print(f"{m0} = {m1} + {m0 - m1}")
+            break
+    else:
+        print('"Goldbach\'s conjecture is wrong."')
