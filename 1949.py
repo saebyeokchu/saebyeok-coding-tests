9c13bacacd1916cb67cf7f35ff136fa704e5a0c4
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-12-19 15:43:40 +0900

    Update 1949.py

--- 1949.py
+++ 1949.py
@@ -1,34 +1,34 @@
-weights = [1000,1000,3000,4000,5000,2000,2000]
+import sys
+sys.setrecursionlimit(10**6)
 
-nums = len(weights)+1
+n = int(sys.stdin.readline().strip())
+weights = [int(x) for x in sys.stdin.readline().split()]
 
-dp = [[0]*nums for i in range(0, nums)]
-dp[1][2] = 1
-dp[2][1] = 1    
-dp[1][3] = 1
-dp[3][1] = 1
-dp[2][6] = 1
-dp[6][2] = 1
-dp[3][5] = 1
-dp[5][3] = 1
-dp[3][4] = 1
-dp[4][3] = 1
+totalNumOfPossibility = int("1"* len(weights),2)
+sumOfPossibility = [0] * (totalNumOfPossibility + 1)
 
-#1번 마을에서 시작
-route = [1]
-isUsu = [0]*nums
-weight = 0
+graph = [ [0] * (len(weights) ) for i in range(0,len(weights)) ]
+for _ in range(n-1):
+    v, u = map(int, sys.stdin.readline().split())
+    graph[v-1][u-1] = 1
+    graph[u-1][v-1] = 1
 
-while len(route) > 0 : 
-    from_ = route.pop()
-    #연결된 중에 우수마을이 있는지 확인
-    for to_ in range(1, nums) :
-        if dp[from_][to_] == 1 :
-            if to_ in isUsu : 
-                search(null, weight) #우수마을이 아닌걸로 설정하고 탐색
-            else :
-                search(null, weight + weights[to_]) #우수마을로 설정하고 탐색
-                search(null, weight) #우수마을이 아닌걸로 설정하고 탐색
-    
-    #우수마을이 있다면 우수마을이 되면 안됨
-    #우수마을이 없다면 우수마을이 될 수도 있고 안될 수도 있다
+def getBinStr(binNum) :
+    return bin(binNum)[2:].zfill(len(weights))
+
+def checkLinkedVillage(villageMap) :
+    for i in range(0, len(villageMap)) :
+        if villageMap[i] == "1" :
+            for j in range(0, len(graph[i])) :
+                if graph[i][j] == 1 and villageMap[j] == "1":
+                    return False
+    return True
+
+for i in range(0, totalNumOfPossibility+1) :
+    binStr = getBinStr(i)[::-1]
+    if checkLinkedVillage(binStr) : #구성 중에 우수마을이 아니여야 하는데 우수마을인 경우 체크
+        for index, char_ in enumerate(binStr) :
+            if char_ == "1"  :
+                sumOfPossibility[i] += weights[index]
+
+print(max(sumOfPossibility))
