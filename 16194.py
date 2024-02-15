2a5440837a0093dfc6748723cadc71bcff49ba1d
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2024-02-15 16:04:52 +0900

    Update 16194.py

--- 16194.py
+++ 16194.py
@@ -5,16 +5,22 @@ d = [0] * (n+1)
 d[n] = p[n]
 
 def find(chase, total):
-
+    
     if chase == n :
         print("total : " , total)
         return
     
-    if chase > n :
-        return
+    print("chase : " , chase)
     
     for i in range(1, n+1) :
-        find(chase+i, total + p[i])
+        if chase + i <= n :
+            if d[chase+i] == 0 :
+                d[chase+i] = total + p[i]
+            elif d[chase+i] > total + p[i]:
+                d[chase+i] = total + p[i]
+            find(chase+i, total + p[i])
 
 for i in range(1, n+1) :
     find(i, p[i])
+
+print(d)
