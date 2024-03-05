e84eb2bb13e0d507b8d1866ff459b049c76fa641
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2024-03-05 21:16:50 +0900

    Update pccp_1_2.py

--- pccp_1_2.py
+++ pccp_1_2.py
@@ -1,1 +1,20 @@
 
+input_list = [[40, 10, 10], [20, 5, 0], [30, 30, 30], [70, 0, 70], [100, 100, 100]]
+
+shows=3
+actors=5
+
+dp = [ [0]*shows for actor in range(0, actors) ]
+
+print(dp)
+
+def search(parentShow, parentActor):
+    for show in range(parentShow,shows) :
+        for actor in range(0,actors) :
+            dp[parentActor][parentShow] = max(input_list[parentActor][parentShow]+input_list[actor][show],dp[parentActor][parentShow])
+
+for show in range(0,shows) :
+        for actor in range(0,actors) :
+            search(show, actor)
+
+print(dp)
